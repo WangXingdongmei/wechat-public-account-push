a1a222a53bd236c84a6d1c05a3858856285e2b6c
WangXingdongmei <113611234+WangXingdongmei@users.noreply.github.com>
2022-09-15 22:43:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,11 +4,55 @@ export const config = {
    */
 
   // 公众号APP_ID
-  APP_ID: "wxcb4e07335004cb15 ",
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
+  APP_ID: "wxcb4e07335004cb15",
+
   // 公众号APP_SECRET
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: "1d5c3b728f47ce02d557af7186867792",
 
-  isShowColor: true,
+  // 是否给文字设置多彩颜色, 和emoji不兼容
+  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
+  IS_SHOW_COLOR: true,
+
+  // 功能开关,打开：true，关闭：false
+  SWITCH: {
+    /** 每日天气 */
+    weather: true,
+
+    /** 节假日 */
+    // 下一休息日综合提醒
+    holidaytts: true,
+
+    /** 每日N句 */
+    // 金山每日一句
+    CIBA: true,
+    // 每日一言
+    oneTalk: false,
+    // 土味情话(彩虹屁)
+    earthyLoveWords: false,
+    // 朋友圈文案
+    momentCopyrighting: false,
+    // 毒鸡汤
+    poisonChickenSoup: false,
+    // 古诗古文
+    poetry: false,
+
+    /** 星座运势 */
+    horoscope: false,
+
+    /** 生日消息和节日消息 */
+    birthdayMessage: true,
+  },
+
+  /** 每日一言 */
+
+  // 每日一言的内容类型
+  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
+  LITERARY_PREFERENCE: "",
+  
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
@@ -20,13 +64,13 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oKC2O6V1ogLEIfg3C5CV_psVYKNI",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "G3Zu9PnO2eWt4eTulLKl2eKf8ZrI2oTMHu3lwAiKrxY",
+      useTemplateId: " 	G3Zu9PnO2eWt4eTulLKl2eKf8ZrI2oTMHu3lwAiKrxY 	",
       // 所在省份
-      province: "云南",
+      province: "广东",
       // 所在城市
-      city: "昆明",
+      city: "肇庆",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '4-28',
+      horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -52,12 +96,12 @@ export const config = {
     },
     {
       name: "老婆1",
-      id: "",
-      useTemplateId: "",
+      id: "oKC2O6V1ogLEIfg3C5CV_psVYKNI",
+      useTemplateId: "G3Zu9PnO2eWt4eTulLKl2eKf8ZrI2oTMHu3lwAiKrxY",
       province: "云南",
       city: "昆明",
-      horoscopeDate: '',
-      horoscopeDateType: '',
+      horoscopeDate: '4-28',
+      horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
       festivals: [],
       customizedDateList: []
@@ -193,12 +237,6 @@ export const config = {
     // 你可以不断按格式往下增加
     // ...
   ],
-
-  /** 每日一言 */
-
-  // 好文节选的内容类型
-  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: ""
 
 
 }
